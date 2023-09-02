68cf7f7d9fffae31316cf8f618f90574e151e840
Rutuja Santikar <santikarrutuja@gmail.com>
2023-09-02 22:08:10 +0530

    pacakges installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      HomeComponent,
+      MoviesListComponent,
+      MoviesDetailsComponent,
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
